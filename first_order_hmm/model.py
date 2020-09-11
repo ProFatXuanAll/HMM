4b8acd036ed746b5dc60af60d0e303b14965c067
ProFatXuanAll <ProFatXuanAll@gmail.com>
2020-09-11 13:46:24 +0800

    add baum-welch algorithm, seperate dumb code from parallel code

--- first_order_hmm/model.py
+++ first_order_hmm/model.py
@@ -11,6 +11,7 @@ from typing import Tuple
 
 import numpy as np
 
+
 class FirstOrderHMM:
     def __init__(
             self,
@@ -22,10 +23,14 @@ class FirstOrderHMM:
     ):
         # Type check.
         if not isinstance(emit_prob_mat, np.ndarray):
-            raise TypeError('`emit_prob_mat` must be an instance of `np.ndarray`.')
+            raise TypeError(
+                '`emit_prob_mat` must be an instance of `np.ndarray`.'
+            )
 
         if not isinstance(init_prob_mat, np.ndarray):
-            raise TypeError('`init_prob_mat` must be an instance of `np.ndarray`.')
+            raise TypeError(
+                '`init_prob_mat` must be an instance of `np.ndarray`.'
+            )
 
         if not isinstance(num_states, int):
             raise TypeError('`num_states` must be an instance of `int`.')
@@ -34,7 +39,9 @@ class FirstOrderHMM:
             raise TypeError('`num_observations` must be an instance of `int`.')
 
         if not isinstance(tran_prob_mat, np.ndarray):
-            raise TypeError('`tran_prob_mat` must be an instance of `np.ndarray`.')
+            raise TypeError(
+                '`tran_prob_mat` must be an instance of `np.ndarray`.'
+            )
 
         # Value check.
         assert num_states >= 1
@@ -42,9 +49,9 @@ class FirstOrderHMM:
         assert init_prob_mat.shape == (num_states,)
         assert emit_prob_mat.shape == (num_states, num_observations)
         assert tran_prob_mat.shape == (num_states, num_states)
-        assert np.all((0 <= init_prob_mat) & (init_prob_mat <= 1))
-        assert np.all((0 <= emit_prob_mat) & (emit_prob_mat <= 1))
-        assert np.all((0 <= tran_prob_mat) & (tran_prob_mat <= 1))
+        assert np.all((init_prob_mat >= 0) & (init_prob_mat <= 1))
+        assert np.all((emit_prob_mat >= 0) & (emit_prob_mat <= 1))
+        assert np.all((tran_prob_mat >= 0) & (tran_prob_mat <= 1))
 
         self.emit_prob_mat = emit_prob_mat
         self.init_prob_mat = init_prob_mat
@@ -57,26 +64,136 @@ class FirstOrderHMM:
         forward_prob_mat = np.zeros((total_time_step, self.num_states))
 
         # Initialize time step 0.
-        forward_prob_mat[0] = self.init_prob_mat * self.emit_prob_mat[:, obs_indices[0]]
+        forward_prob_mat[0] = self.init_prob_mat * \
+            self.emit_prob_mat[:, obs_indices[0]]
 
-        # Iteratively calculating forward probabilities \alpha_t(i).
+        # Iteratively calculate forward probabilities \alpha_t(i).
         for time_step in range(1, total_time_step):
-            forward_prob_vec = np.expand_dims(forward_prob_mat[time_step - 1], axis=-1)
-            forward_prob_vec = forward_prob_vec * self.tran_prob_mat
-            forward_prob_vec = forward_prob_vec.sum(axis=0)
-            forward_prob_vec = forward_prob_vec * self.emit_prob_mat[:, obs_indices[time_step]]
-            forward_prob_mat[time_step] = forward_prob_vec
+            forward_prob_vec = (
+                forward_prob_mat[time_step - 1].reshape(-1, 1) *
+                self.tran_prob_mat
+            ).sum(axis=0)
+            forward_prob_mat[time_step] = (
+                forward_prob_vec *
+                self.emit_prob_mat[:, obs_indices[time_step]]
+            )
 
         return forward_prob_mat
 
-    def forward_dumb(self, obs_indices: np.ndarray) -> np.ndarray:
+    def obs_prob_by_forward(self, obs_indices: np.ndarray) -> float:
+        forward_prob_mat = self.forward(obs_indices=obs_indices)
+        return forward_prob_mat[-1].sum()
+
+    def backward(self, obs_indices: np.ndarray) -> np.ndarray:
+        total_time_step = obs_indices.shape[0]
+        backward_prob_mat = np.zeros((total_time_step, self.num_states))
+
+        # Initialize last time step T.
+        backward_prob_mat[-1] = 1
+
+        # Iteratively calculate backward probabilities \beta_t(i).
+        for time_step in range(total_time_step - 2, -1, -1):
+            backward_prob_mat[time_step] = (
+                self.tran_prob_mat *
+                self.emit_prob_mat[:, obs_indices[time_step + 1]].reshape(1, -1) *
+                backward_prob_mat[time_step + 1].reshape(1, -1)
+            ).sum(axis=1)
+
+        return backward_prob_mat
+
+    def obs_prob_by_backward(self, obs_indices: np.ndarray) -> float:
+        backward_prob_mat = self.backward(obs_indices=obs_indices)
+        return (
+            self.init_prob_mat *
+            self.emit_prob_mat[:, obs_indices[0]] *
+            backward_prob_mat[0]
+        ).sum()
+
+    def viterbi(
+            self, obs_indices: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        total_time_step = obs_indices.shape[0]
+        viterbi_prob_mat = np.zeros((total_time_step, self.num_states))
+        prev_state_mat = np.zeros(
+            (total_time_step, self.num_states), dtype=np.int64)
+
+        # Initialize time step 0.
+        viterbi_prob_mat[0] = self.init_prob_mat * \
+            self.emit_prob_mat[:, obs_indices[0]]
+
+        # Iteratively calculate Viterbi probabilities \delta_t(i) and previous best state
+        # \psi_t(i).
+        for time_step in range(1, total_time_step):
+            viterbi_prob_vec = (
+                viterbi_prob_mat[time_step - 1].reshape(-1, 1) *
+                self.tran_prob_mat
+            )
+            prev_state_mat[time_step] = np.argmax(viterbi_prob_vec, axis=0)
+            viterbi_prob_vec = np.max(viterbi_prob_vec, axis=0)
+            viterbi_prob_mat[time_step] = (
+                viterbi_prob_vec *
+                self.emit_prob_mat[:, obs_indices[time_step]]
+            )
+
+        # Back track all best states.
+        all_best_states = [np.argmax(prev_state_mat[-1])]
+        for time_step in range(total_time_step - 2, -1, -1):
+            all_best_states.append(
+                prev_state_mat[time_step, all_best_states[-1]])
+
+        all_best_states.reverse()
+
+        return viterbi_prob_mat, np.array(all_best_states)
+
+    def baum_welch(self, obs_indices: np.ndarray) -> None:
+        total_time_step = obs_indices.shape[0]
+        forward_prob_mat = self.forward(obs_indices=obs_indices)
+        backward_prob_mat = self.backward(obs_indices=obs_indices)
+        obs_prob = self.obs_prob_by_forward(obs_indices=obs_indices)
+
+        # Iteratively calculate expected number of times transition from state i to state j at time
+        # step t \xi_t(i, j). Also calculate expected number of times transistion from state i at
+        # time step t \gamma_t(i).
+        exp_from_i_to_j = np.zeros(
+            (total_time_step - 1, self.num_states, self.num_states))
+        exp_from_i = np.zeros((total_time_step, self.num_states))
+
+        for time_step in range(total_time_step - 1):
+            exp_from_i_to_j[time_step] = (
+                forward_prob_mat[time_step].reshape(-1, 1) *
+                self.tran_prob_mat *
+                self.emit_prob_mat[:, obs_indices[time_step + 1]].reshape(1, -1) *
+                backward_prob_mat[time_step + 1].reshape(1, -1)
+            ) / obs_prob
+            exp_from_i[time_step] = exp_from_i_to_j[time_step].sum(axis=-1)
+
+        exp_from_i[-1] = forward_prob_mat[-1] * \
+            backward_prob_mat[-1] / obs_prob
+
+        # Update parameters.
+        self.init_prob_mat = exp_from_i[0]
+        self.tran_prob_mat = (
+            exp_from_i_to_j.sum(axis=0) /
+            exp_from_i[:-1].sum(axis=0).reshape(-1, 1)
+        )
+        self.emit_prob_mat = (
+            exp_from_i.T @ (
+                obs_indices.reshape(-1, 1) ==
+                np.arange(self.num_observations).reshape(1, -1)
+            ) /
+            exp_from_i.sum(axis=0).reshape(-1, 1)
+        )
+
+
+class FirstOrderHMMDumb(FirstOrderHMM):
+    def forward(self, obs_indices: np.ndarray) -> np.ndarray:
         total_time_step = obs_indices.shape[0]
         forward_prob_mat = np.zeros((total_time_step, self.num_states))
 
         # Initialize time step 0.
-        forward_prob_mat[0] = self.init_prob_mat * self.emit_prob_mat[:, obs_indices[0]]
+        forward_prob_mat[0] = self.init_prob_mat * \
+            self.emit_prob_mat[:, obs_indices[0]]
 
-        # Iteratively calculating forward probabilities \alpha_t(i).
+        # Iteratively calculate forward probabilities \alpha_t(i).
         for time_step in range(1, total_time_step):
             for j in range(self.num_states):
                 forward_prob = 0.0
@@ -101,26 +218,7 @@ class FirstOrderHMM:
         # Initialize last time step T.
         backward_prob_mat[-1] = 1
 
-        # Iteratively calculating backward probabilities \beta_t(i).
-        for time_step in range(total_time_step - 2, -1, -1):
-            backward_prob_vec = (
-                self.tran_prob_mat *
-                np.expand_dims(self.emit_prob_mat[:, obs_indices[time_step + 1]], axis=0) *
-                np.expand_dims(backward_prob_mat[time_step + 1], axis=0)
-            )
-            backward_prob_vec = backward_prob_vec.sum(axis=1)
-            backward_prob_mat[time_step] = backward_prob_vec
-
-        return backward_prob_mat
-
-    def backward_dumb(self, obs_indices: np.ndarray) -> np.ndarray:
-        total_time_step = obs_indices.shape[0]
-        backward_prob_mat = np.zeros((total_time_step, self.num_states))
-
-        # Initialize last time step T.
-        backward_prob_mat[-1] = 1
-
-        # Iteratively calculating backward probabilities \beta_t(i).
+        # Iteratively calculate backward probabilities \beta_t(i).
         for time_step in range(total_time_step - 2, -1, -1):
             for i in range(self.num_states):
                 backward_prob = 0.0
@@ -134,47 +232,18 @@ class FirstOrderHMM:
 
         return backward_prob_mat
 
-
-    def viterbi(self, obs_indices: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    def viterbi(
+            self, obs_indices: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         total_time_step = obs_indices.shape[0]
         viterbi_prob_mat = np.zeros((total_time_step, self.num_states))
-        prev_state_mat = np.zeros((total_time_step, self.num_states), dtype=np.int64)
+        prev_state_mat = np.zeros(
+            (total_time_step, self.num_states), dtype=np.int64)
 
         # Initialize time step 0.
-        viterbi_prob_mat[0] = self.init_prob_mat * self.emit_prob_mat[:, obs_indices[0]]
+        viterbi_prob_mat[0] = self.init_prob_mat * \
+            self.emit_prob_mat[:, obs_indices[0]]
 
-        # Iteratively calculating viterbi probabilities \delta_t(i) and previous best state
-        # \psi_t(i).
-        for time_step in range(1, total_time_step):
-            viterbi_prob_vec = (
-                np.expand_dims(viterbi_prob_mat[time_step - 1], axis=-1) *
-                self.tran_prob_mat
-            )
-            prev_state_mat[time_step] = np.argmax(viterbi_prob_vec, axis=0)
-            viterbi_prob_vec = np.max(viterbi_prob_vec, axis=0)
-            viterbi_prob_mat[time_step] = (
-                viterbi_prob_vec *
-                self.emit_prob_mat[: , obs_indices[time_step]]
-            )
-
-        # Back track all best states.
-        all_best_states = [np.argmax(prev_state_mat[-1])]
-        for time_step in range(total_time_step - 2, -1, -1):
-            all_best_states.append(prev_state_mat[time_step, all_best_states[-1]])
-
-        all_best_states.reverse()
-
-        return viterbi_prob_mat, np.array(all_best_states)
-
-    def viterbi_dumb(self, obs_indices: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        total_time_step = obs_indices.shape[0]
-        viterbi_prob_mat = np.zeros((total_time_step, self.num_states))
-        prev_state_mat = np.zeros((total_time_step, self.num_states), dtype=np.int64)
-
-        # Initialize time step 0.
-        viterbi_prob_mat[0] = self.init_prob_mat * self.emit_prob_mat[:, obs_indices[0]]
-
-        # Iteratively calculating viterbi probabilities \delta_t(i) and previous best state
+        # Iteratively calculate Viterbi probabilities \delta_t(i) and previous best state
         # \psi_t(i).
         for time_step in range(1, total_time_step):
             for j in range(self.num_states):
@@ -200,8 +269,50 @@ class FirstOrderHMM:
         # Back track all best states.
         all_best_states = [np.argmax(prev_state_mat[-1])]
         for time_step in range(total_time_step - 2, -1, -1):
-            all_best_states.append(prev_state_mat[time_step, all_best_states[-1]])
+            all_best_states.append(
+                prev_state_mat[time_step, all_best_states[-1]])
 
         all_best_states.reverse()
 
         return viterbi_prob_mat, np.array(all_best_states)
+
+    def baum_welch(self, obs_indices: np.ndarray) -> None:
+        total_time_step = obs_indices.shape[0]
+        forward_prob_mat = self.forward(obs_indices=obs_indices)
+        backward_prob_mat = self.backward(obs_indices=obs_indices)
+        obs_prob = self.obs_prob_by_forward(obs_indices=obs_indices)
+
+        # Iteratively calculate expected number of times transition from state i to state j at time
+        # step t \xi_t(i, j). Also calculate expected number of times transistion from state i at
+        # time step t \gamma_t(i).
+        exp_from_i_to_j = np.zeros(
+            (total_time_step - 1, self.num_states, self.num_states))
+        exp_from_i = np.zeros((total_time_step, self.num_states))
+
+        for time_step in range(total_time_step - 1):
+            for i in range(self.num_states):
+                for j in range(self.num_states):
+                    exp_from_i_to_j[time_step, i, j] = (
+                        forward_prob_mat[time_step, i] *
+                        self.tran_prob_mat[i, j] *
+                        self.emit_prob_mat[j, obs_indices[time_step + 1]] *
+                        backward_prob_mat[time_step + 1, j]
+                    ) / obs_prob
+                exp_from_i[time_step, i] = exp_from_i_to_j[time_step, i].sum()
+
+        exp_from_i[-1] = forward_prob_mat[-1] * \
+            backward_prob_mat[-1] / obs_prob
+
+        # Update parameters.
+        for i in range(self.num_states):
+            self.init_prob_mat[i] = exp_from_i[0, i]
+            for j in range(self.num_states):
+                self.tran_prob_mat[i, j] = (
+                    exp_from_i_to_j[:, i, j].sum() /
+                    exp_from_i[:-1, i].sum()
+                )
+            for k in range(self.num_observations):
+                self.emit_prob_mat[i, k] = (
+                    ((obs_indices == k) * exp_from_i[:, i]).sum() /
+                    exp_from_i[:, i].sum()
+                )
